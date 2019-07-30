6956e6d4fca65bb514ed9d43d2753bf1f8dadc55
DoReVo <frz565@gmail.com>
2019-07-30 22:52:12 +0800

    1. Created Transaction Model 2. Defined relationships with category,items and detail.

---
+++ app/Transaction.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Category;
+use App\Item;
+use App\Detail;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Transaction extends Model
+{
+    use SoftDeletes;
+    protected $table = 'finance_transaction';
+    protected $primaryKey = 'id';
+
+    const CREATED_AT = "record_date";
+    const UPDATED_AT = 'update_date';
+
+    protected $fillable = [
+    'date_time',
+    'amount'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo('App\Category', 'category', 'id');
+    }
+
+    public function item()
+    {        
+        return $this->hasMany('App\Item', 'transaction_id', 'id');
+    }
+
+    public function detail()
+    {
+        return $this->hasOne('App\Detail', 'transaction_id', 'id');
+    }
+}
