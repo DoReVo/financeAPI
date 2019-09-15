edc0c5e6c16494f382b675086437d43741204137
DoReVo <frz565@gmail.com>
2019-09-16 01:00:33 +0800

    Deleted Unused route

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
-
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
@@ -39,12 +28,8 @@ $router->group(
             'transaction/{id}',
             ['uses' => 'TransactionController@deleteTransaction']
         );
-        $router->put(
-            'transaction/{id}',
-            ['uses' => 'TransactionController@replaceTransaction']
-        );
         $router->patch(
-            'transaction/{id:\d+}/{column:date_time|category|amount}',            
+            'transaction/{id:\d+}/{column:date_time|category|amount}',
             ['uses'=>'TransactionController@editTransaction']
         );
         $router->patch(
