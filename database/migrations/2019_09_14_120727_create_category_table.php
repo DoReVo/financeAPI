ff144a26c006b272614386f8d1a80ed50d53d8dc
DoReVo <frz565@gmail.com>
2019-09-15 09:56:57 +0800

    added category table migration

---
+++ database/migrations/2019_09_14_120727_create_category_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('finance_category', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('category_name', 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('finance_category');
+    }
+}
