13234ce6f969226a33144a21606ee13d996e2f3b
Izzat <frz565@gmail.com>
2020-01-09 03:24:34 +0800

    Linting

--- routes/web.php
+++ routes/web.php
@@ -4,22 +4,25 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-
 $router->group(
-    ['prefix'=>'api/','middleware' => 'cors'],
+    ['prefix' => 'api/', 'middleware' => 'verify', 'cors'],
     function () use ($router) {
+        // Get all transaction
         $router->get(
             'transaction',
             ['uses' => 'TransactionController@getAllTransaction']
         );
+        // Get 1 transaction
         $router->get(
             'transaction/{id}',
             ['uses' => 'TransactionController@getOneTransaction']
         );
+        // Create a transaction
         $router->post(
             'transaction',
             ['uses' => 'TransactionController@createTransaction']
         );
+        // Create a transaction's item
         $router->post(
             'transaction/{id:\d+}/item',
             ['uses' => 'TransactionController@createTransactionItem']
@@ -31,7 +34,7 @@ $router->group(
         );
         // Delete transaction
         $router->delete(
-            'transaction/{id}',
+            'transaction/{id:\d+}',
             ['uses' => 'TransactionController@deleteTransaction']
         );
         // Delete category
@@ -44,14 +47,18 @@ $router->group(
         // Edit transaction
         $router->patch(
             'transaction/{id:\d+}/{column:date_time|category|amount}',
-            ['uses'=>'TransactionController@editTransaction']
+            ['uses' => 'TransactionController@editTransaction']
         );
+        // Edit transaction detail
         $router->patch(
             'transaction/{id:\d+}/detail',
-            ['uses'=>'TransactionController@editTransactionDetail']
+            ['uses' => 'TransactionController@editTransactionDetail']
         );
+        // Edit transaction Item
         $router->patch(
             'transaction/{id:\d+}/item/{itemId:\d+}/{column:item_name|item_amount|unit_price}',
+            ['uses' => 'TransactionController@editTransactionItem']
+        );
         //Edit Category
         $router->patch(
             'category/{id:\d+}',
