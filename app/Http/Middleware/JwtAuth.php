ef9cbce77ff641b496fadef6b9cadf3bdb3d7b3c
Izzat <frz565@gmail.com>
2020-01-09 09:56:40 +0800

    Linting

--- app/Http/Middleware/JwtAuth.php
+++ app/Http/Middleware/JwtAuth.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Http\Middleware;
-use \Firebase\JWT\JWT;
+
 use Closure;
+use \Firebase\JWT\JWT;
+
 // use \DateTime;
 // use \DateInterval;
 
@@ -16,15 +18,13 @@ class JwtAuth
      * @return mixed
      */
 
-    
-
     public function handle($request, Closure $next)
     {
-      // Public Key from env
+        // Public Key from env
         $publicKey = getenv('JWT_PUBLIC_KEY');
 
         $token = $request->bearerToken();
- 
+
         if (!$token) {
             return response(
                 json_encode(
@@ -35,10 +35,10 @@ class JwtAuth
                 401
             );
         }
-         
+
         try {
             $decoded = JWT::decode($token, $publicKey, array('RS256'));
-         return $next($request);
+            return $next($request);
         } catch (\Exception $e) {
             return response(
                 json_encode(
@@ -48,60 +48,27 @@ class JwtAuth
                 ),
                 403
             );
-            // return $e->getMessage();
-       }
-       catch(\Exception $e){
-         // echo 'FAILED';
-         echo $e->getMessage();
-       }
 
-        // return response()->json($request->all());
+        }
     }
 
-    // public function __construct(Request $request)
-    // {
-    //     $this->token = $request->all();
-    //     $this->jwt = JWT::encode($this->token,$this->key);
-    // }
+    public function generateToken($user)
+    {
+        $key = 'keyPair';
 
-    //
+        $date = new \DateTime();
+        $date->add(new \DateInterval('P1D'));
+        // $date->add(new DateInterval('P1D'));
+        $token = array(
+            "iss" => "pcogd",
+            "sub" => $user->id,
+            "aud" => "pcogdfm",
+            "exp" => intval($date->format('U')),
+            "iat" => time(),
+        );
 
-    public function verifyToken($request){
-      $key = 'heilHitler';
+        $jwt = JWT::encode($token, $key);
 
-     $token= $request->input('jwtToken');
-
-      try{
-        
-        $decoded = JWT::decode($token, 'Fake', array('HS256'));
-        return $decoded;
-      
-      }
-      catch(\Exception $e){
-        // echo 'FAILED';
-        echo $e->getMessage();
-      }
-       
-      
-    }
-
-    public function generateToken($user){
-      $key = 'keyPair';
-     
-      $date = new \DateTime();
-      $date->add(new \DateInterval('P1D'));
-      // $date->add(new DateInterval('P1D'));
-      $token = array(
-        "iss"=>"pcogd",
-        "sub"=> $user->id,
-        "aud"=> "pcogdfm",
-        "exp"=> intval($date->format('U')),
-        "iat"=>time()
-      );
-
-      $jwt = JWT::encode($token,$key);
-
-      return $jwt;
-      
+        return $jwt;
     }
 }
